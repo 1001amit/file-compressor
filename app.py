0a0ba214bcfd0fb3a3e1b754d4656cbfef89a0c6
AMIT <amit12589630@gmail.com>
2024-07-21 10:31:33 +0530

    update

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, send_file
+from flask import Flask, render_template, request, send_file, redirect, url_for, flash
 from werkzeug.utils import secure_filename
 import zipfile
 import os
@@ -6,6 +6,7 @@ import os
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = 'uploads'
 app.config['COMPRESSED_FOLDER'] = 'compressed'
+app.config['SECRET_KEY'] = 'supersecretkey'
 
 os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
 os.makedirs(app.config['COMPRESSED_FOLDER'], exist_ok=True)
@@ -16,17 +17,26 @@ def index():
 
 @app.route('/compress', methods=['POST'])
 def compress():
-    uploaded_files = request.files.getlist('files[]')
-    zip_filename = os.path.join(app.config['COMPRESSED_FOLDER'], 'compressed.zip')
+    try:
+        uploaded_files = request.files.getlist('files[]')
+        if not uploaded_files or uploaded_files[0].filename == '':
+            flash('No files selected for uploading', 'error')
+            return redirect(url_for('index'))
 
-    with zipfile.ZipFile(zip_filename, 'w') as zipf:
-        for file in uploaded_files:
-            filename = secure_filename(file.filename)
-            file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-            file.save(file_path)
-            zipf.write(file_path, filename)
+        zip_filename = os.path.join(app.config['COMPRESSED_FOLDER'], 'compressed.zip')
 
-    return send_file(zip_filename, as_attachment=True)
+        with zipfile.ZipFile(zip_filename, 'w') as zipf:
+            for file in uploaded_files:
+                filename = secure_filename(file.filename)
+                file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+                file.save(file_path)
+                zipf.write(file_path, filename)
+
+        return send_file(zip_filename, as_attachment=True)
+
+    except Exception as e:
+        flash(f'An error occurred: {e}', 'error')
+        return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.run(debug=True)
